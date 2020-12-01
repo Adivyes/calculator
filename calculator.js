de245cb8e26a250da640d14a4aedbfe45e46090b
Adivyes <Adivyes@gmail.com>
2020-12-01 13:10:00 +0200

    init

---
+++ calculator.js
@@ -0,0 +1,57 @@
+ window.onload = ()=>{
+   onecal.addEventListener('click', ()=>{calcu.value += 1})
+   twocal.addEventListener('click', ()=>{calcu.value += 2})
+   treecal.addEventListener('click', ()=>{calcu.value += 3})
+   fourcal.addEventListener('click', ()=>{calcu.value += 4})
+   fivecal.addEventListener('click', ()=>{calcu.value += 5})
+   sixcal.addEventListener('click', ()=>{calcu.value += 6})
+   sevencal.addEventListener('click', ()=>{calcu.value += 7})
+   eightcal.addEventListener('click', ()=>{calcu.value += 8})
+   ninecal.addEventListener('click', ()=>{calcu.value += 9})
+   zerocal.addEventListener('click', ()=>{calcu.value += 0})
+   mincal.addEventListener('click', ()=>{calcu.value += "-"})
+   sumcal.addEventListener('click', ()=>{calcu.value += "+"})
+   divical.addEventListener('click', ()=>{calcu.value += '/'})
+   decemelcal.addEventListener('click', ()=>{calcu.value += '.'})
+   multical.addEventListener('click', ()=>{calcu.value += '*'})
+   ACcal.addEventListener('click', ()=>{calcu.value = ' '})
+ }
+
+function takeNum(){
+
+}
+
+
+
+
+// // function messegeNum(num){
+//     switch (num){
+
+//         case 1:
+//             messagebtn.innerHTML = 
+//             break;
+//         case 2:
+//             messagebtn.innerHTML = 
+//             break;
+//         case 3:
+//             messagebtn.innerHTML = 
+//             break;
+//         case 4:
+//             messagebtn.innerHTML = 
+//             break;
+//         case 5:
+//             messagebtn.innerHTML = 
+//             break;
+//         case 6:
+//             messagebtn.innerHTML = 
+//             break;
+//         case 7:
+//             messagebtn.innerHTML = 
+//             break;
+//         case 8:
+//             messagebtn.innerHTML = 
+//             break;
+//             default:
+//                 messagebtn.innerHTML = `n/v`
+//     }
+// }
