fef7119553e387c780d975d743141d447019200f
Adivyes <Adivyes@gmail.com>
2020-12-07 14:39:51 +0200

    init

--- homework.js
+++ homework.js
@@ -1,26 +1,26 @@
-window.onload = function() {
-    var x = document.getElementById("main");
-    x.style.color = '#6600FF';
-    x.style.fontSize = '50px';
-};
+// window.onload = function() {
+//     var x = document.getElementById("main");
+//     x.style.color = '#6600FF';
+//     x.style.fontSize = '50px';
+// };
 
-class Ranum {
-    name;
-    lastname;
-    number;
-    constructor(name,lastname,number){
-        this.name = name;
-        this.lastname = lastname;
-        this.number = number;
-    }
-    static randomnum (number1){
+// class Ranum {
+//     name;
+//     lastname;
+//     number;
+//     constructor(name,lastname,number){
+//         this.name = name;
+//         this.lastname = lastname;
+//         this.number = number;
+//     }
+//     static randomnum (number1){
 
-    return Math.floor(Math.random()*number1.number)
-    }
-}
-let p1 = new Ranum ("adiv","shenkor",1000);
-Ranum.randomnum(p1)
-console.log(Ranum.randomnum(p1));
+//     return Math.floor(Math.random()*number1.number)
+//     }
+// }
+// let p1 = new Ranum ("adiv","shenkor",1000);
+// Ranum.randomnum(p1)
+// console.log(Ranum.randomnum(p1));
 // class User1{
 //     name;
 //     password;
@@ -86,3 +86,84 @@ console.log(Ranum.randomnum(p1));
 // }
 // const stu1 = new Student(7,"dsgfdsgd","Yossi", 16, 160);
 // stu1.speak()
+// static bestEngine(array1){
+//     let max = 0;
+//     let maxIndex = 0;
+//     for (let i = 0; i < array1.length; i++) {
+// if(array1[i].engine_capacity > max){
+//     max = array1[i].engine_capacity;
+//     maxIndex = i;
+// }   
+//     }
+//     return array1[maxIndex];
+// }
+
+class Vehicles{
+    engine_capacity;
+    wheels_Number;
+    color;
+    constructor(engine_capacity,wheels_Number,color){
+        this.engine_capacity = engine_capacity;
+        this.wheels_Number = wheels_Number;
+        this.color = color;
+    }
+static bestEngine(array1){
+let max = 0;
+let maxIndex = 0;
+for (let i = 0; i < array1.length; i++) {
+    if(array1[i].engine_capacity > max){
+        max = array1[i].engine_capacity;
+        maxIndex = i;
+    }
+}
+return array1[maxIndex];
+}
+    printreturn(){
+        return `this vehicle have engine capacity of: ${this.engine_capacity}/n  with the number of wheels of:  ${this.wheels_Number}/n and have the color: ${this.colorw}`;
+    }
+}
+class Car extends Vehicles{
+    company;
+    constructor(company,engine_capacity,wheels_Number,color){
+        super(engine_capacity,wheels_Number,color);
+        this.company = company;
+    }
+    get lettersBig(){
+        return this.bigLetters();
+    }
+
+    bigLetters(){
+       return this.company.toUpperCase()
+    }
+    printCar(){
+        return `the name of the company is: ${this.company}/n and ${super.printreturn()}`;
+    }
+}
+class Truck extends Vehicles{
+    size;
+    constructor(size,engine_capacity,wheels_Number,color){
+        super(engine_capacity,wheels_Number,color);
+        this.size = size;
+    }
+    printTruck(){
+        return `the kind of this truck is ${this.size}/n and ${super.printreturn()}`;
+    }
+}
+class Motorcicel extends Vehicles{
+    gears_type;
+    constructor(gears_type,engine_capacity,wheels_Number,color){
+        super(engine_capacity,wheels_Number,color)
+        this.gears_type = gears_type
+    }
+    printMotorcicel(){
+        return `the gear type of this vehicles is ${this.gears_type}/n and ${super.printreturn()}`;
+    }
+}
+let prinItMotor = new Motorcicel('auto',225,2,'red');
+let prinItTruck = new Truck('semi triler',6000,12,'black');
+let prinItCar = new Car('toyota',1400,4,'blue');
+
+
+
+
+console.log(Vehicles.bestEngine([prinItMotor,prinItTruck,prinItCar]));
